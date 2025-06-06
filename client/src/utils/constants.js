e1d993d6b15e4251d6b0a174aed0a7935cfd04f3
Kriti <kritibhatt09@gmail.com>
2025-06-06 22:00:45 +0530

    updated constants.js

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
 import abi from './Transactions.json'
 
 export const contractABI = abi.abi;
-export const contractAddress = '0x2eCB703923DB78114B06Db837A9b93318951688d';
+export const contractAddress = import.meta.env.VITE_APP_CONTRACT_ADDRESS;
